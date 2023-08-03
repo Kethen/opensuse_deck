a567aa5592a835667382c99319ede78d6a69d4db
Katharine Chui <kwchuiaa@connect.ust.hk>
2023-08-03 21:16:19 +0800

    fix backlight node probing

--- overlay/usr/libexec/deck_permissions.sh
+++ overlay/usr/libexec/deck_permissions.sh
@@ -1,7 +1,12 @@
 tgt_group=deck
 
-for f in /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon*/power*_cap /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/drm/card1/card1-eDP-1/amdgpu_bl1/brightness /sys/class/drm/card*/device/power_dpm_force_performance_level /sys/class/drm/card*/device/pp_od_clk_voltage
+for f in /sys/devices/pci0000:00/0000:00:08.1/0000:04:00.0/hwmon/hwmon*/power*_cap /sys/class/backlight/amdgpu_bl*/brightness /sys/class/drm/card*/device/power_dpm_force_performance_level /sys/class/drm/card*/device/pp_od_clk_voltage
 do
-	chmod 664 $f
-	chown 0:$tgt_group $f
+	if [ -e "$f" ]
+	then
+		chmod 664 "$f"
+		chown 0:$tgt_group "$f"
+	else
+		echo skipping "$f" because it does not exist
+	fi
 done
