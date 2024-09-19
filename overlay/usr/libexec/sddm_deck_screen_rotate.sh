642ab6091c59e544f9431273095b656d8b949a1a
Katharine Chui <kwchuiaa@connect.ust.hk>
2024-09-19 15:24:03 +0200

    only help x11 sddm rotate on lcd steamdeck

--- overlay/usr/libexec/sddm_deck_screen_rotate.sh
+++ overlay/usr/libexec/sddm_deck_screen_rotate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-xrandr --output eDP-1 --rotate right
-xinput set-prop 'FTS3528:00 2808:1015' --type=float 'Coordinate Transformation Matrix'  0 1 0 -1 0 1 0 0 1
+
+DMIPATH=/sys/class/dmi/id
+BOARD=$(cat $DMIPATH/board_name)
+PRODUCT=$(cat $DMIPATH/product_name)
+VENDOR=$(cat $DMIPATH/sys_vendor)
+
+if [ "$VENDOR" == "Valve" ] && [ "$BOARD" == "Jupiter" ]
+then
+	xrandr --output eDP-1 --rotate right
+	xinput set-prop 'FTS3528:00 2808:1015' --type=float 'Coordinate Transformation Matrix'  0 1 0 -1 0 1 0 0 1
+fi
